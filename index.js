b12de75e9674828c62e5a86594997350401b0f78
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-06-27 19:06:32 +0200

    Create basic files

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+app.use(express.static('assets'));
+
+app.get('/', function(req, res) {
+	res.sendFile('/index.html');
+});
+
+app.get('/userform', function(req, res) {
+	const response = {
+		first_name: req.query.first_name,
+        last_name: req.query.last_name,
+        email_adress: req.query.email_adress,
+        password: req.query.password
+	};
+	res.json(response);
+});
+
+var server = app.listen(3000, 'localhost', function() {
+	var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
